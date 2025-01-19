8149c65004ee66b995bfc1d44f84ed6693220c37
11929091 <159616580+11929091@users.noreply.github.com>
2025-01-19 16:34:33 +0900

    【分類情報管理：DB操作 ①】分類情報管理画面の初期表示に出力する分類名データをDBから取得

--- DroneInventorySystem/src/main/java/com/digitalojt/web/controller/CategorizedInfoMngController.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/controller/CategorizedInfoMngController.java
@@ -2,20 +2,16 @@ package com.digitalojt.web.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import com.digitalojt.web.consts.ItemCategory;
 import com.digitalojt.web.consts.UrlConsts;
 import com.digitalojt.web.dto.SearchForm;
-
-import jakarta.validation.Valid;
+import com.digitalojt.web.entity.CategoryInfo;
+import com.digitalojt.web.service.CategorizedInfoMngService;
 
 /**
  * 分割情報管理画面コントローラークラス
@@ -26,8 +22,12 @@ import jakarta.validation.Valid;
 @Controller
 public class CategorizedInfoMngController extends AbstractController {
 
-	// 分類名リスト初期化
-	private List<String> categoryList = new ArrayList<>();
+	/** 分類情報テーブル リポジトリー */
+	@Autowired
+	private CategorizedInfoMngService service;
+
+	/** 分類名リスト初期化 */
+	private List<CategoryInfo> categoryList = new ArrayList<>();
 
 	/**
 	 * 初期表示
@@ -40,7 +40,8 @@ public class CategorizedInfoMngController extends AbstractController {
 
 		// 分類名リストが空だったら分類名をリストに格納
 		if (categoryList.isEmpty()) {
-			inputCategoryList();
+			// 分類情報管理画面に表示するデータを取得
+			categoryList = service.getCategoryInfoData();
 		}
 
 		model.addAttribute("itemCategory", categoryList);
@@ -49,47 +50,47 @@ public class CategorizedInfoMngController extends AbstractController {
 		return "admin/categorizedInfoMng/index";
 	}
 
-	/**
-	 * 分類名検索
-	 * 
-	 * @param searchForm 検索用フォーム
-	 * @param result 入力チェック
-	 * @param model モデル
-	 * @return
-	 */
-	@PostMapping("/searchCategory")
-	public String searchCategory(@Valid @ModelAttribute("searchForm") SearchForm searchForm,
-			BindingResult result, Model model) {
-		
-		if(result.hasErrors()) {
-			model.addAttribute("itemCategory", categoryList);
+	//	/**
+	//	 * 分類名検索
+	//	 * 
+	//	 * @param searchForm 検索用フォーム
+	//	 * @param result 入力チェック
+	//	 * @param model モデル
+	//	 * @return
+	//	 */
+	//	@PostMapping("/searchCategory")
+	//	public String searchCategory(@Valid @ModelAttribute("searchForm") SearchForm searchForm,
+	//			BindingResult result, Model model) {
+	//		
+	//		if(result.hasErrors()) {
+	//			model.addAttribute("itemCategory", categoryList);
+	//
+	//			return "admin/categorizedInfoMng/index";
+	//		}
+	//
+	//		String keyword = searchForm.getSearchKeyword();
+	//		List<CategoryInfo> filteredCategories = categoryList.stream()
+	//				.filter(category -> category.contains(keyword))
+	//				.collect(Collectors.toList());
+	//
+	//		// 検索結果をModelに格納
+	//		model.addAttribute("itemCategory", filteredCategories);
+	//		return "admin/categorizedInfoMng/index";
+	//	}
 
-			return "admin/categorizedInfoMng/index";
-		}
-
-		String keyword = searchForm.getSearchKeyword();
-		List<String> filteredCategories = categoryList.stream()
-				.filter(category -> category.contains(keyword))
-				.collect(Collectors.toList());
-
-		// 検索結果をModelに格納
-		model.addAttribute("itemCategory", filteredCategories);
-		return "admin/categorizedInfoMng/index";
-	}
-
-	/**
-	 * 分類名リスト格納
-	 */
-	public void inputCategoryList() {
-		categoryList.add(ItemCategory.FRAME);
-		categoryList.add(ItemCategory.PROPELLER);
-		categoryList.add(ItemCategory.ELECTRIC_MOTOR);
-		categoryList.add(ItemCategory.ELECTRONIC_SPEED_CONTROLLER);
-		categoryList.add(ItemCategory.BATTERY);
-		categoryList.add(ItemCategory.FLIGHT_CONTROLLER);
-		categoryList.add(ItemCategory.REMOTE_CONTROLLER);
-		categoryList.add(ItemCategory.RECEIVER);
-		categoryList.add(ItemCategory.GPS_MODULE);
-		categoryList.add(ItemCategory.CAMERA_SENSOR);
-	}
+	//	/**
+	//	 * 分類名リスト格納
+	//	 */
+	//	public void inputCategoryList() {
+	//		categoryList.add(ItemCategory.FRAME);
+	//		categoryList.add(ItemCategory.PROPELLER);
+	//		categoryList.add(ItemCategory.ELECTRIC_MOTOR);
+	//		categoryList.add(ItemCategory.ELECTRONIC_SPEED_CONTROLLER);
+	//		categoryList.add(ItemCategory.BATTERY);
+	//		categoryList.add(ItemCategory.FLIGHT_CONTROLLER);
+	//		categoryList.add(ItemCategory.REMOTE_CONTROLLER);
+	//		categoryList.add(ItemCategory.RECEIVER);
+	//		categoryList.add(ItemCategory.GPS_MODULE);
+	//		categoryList.add(ItemCategory.CAMERA_SENSOR);
+	//	}
 }
